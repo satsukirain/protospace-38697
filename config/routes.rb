9f8e84cf59bad5e49de157fd5d6f2f01210cd31e
satsukirain <117341700+satsukirain@users.noreply.github.com>
2022-12-06 18:14:37 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # get 'prototypes/index'
+  root to:"prototypes#index"
+  resources :prototypes, only: [:new, :create, :destroy, :edit, :update, :show] do
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
+end
